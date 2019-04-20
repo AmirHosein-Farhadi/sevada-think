9082363d4aa0ec1ad545c88b58ae195e47bfe4bb
Amir <cnooed@gmail.com>
2019-04-20 09:29:24 -0400

    initial commit

---
+++ src/main/java/com/cyberoxi/sevadathink/SevadaThinkApplication.java
@@ -0,0 +1,13 @@
+package com.cyberoxi.sevadathink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevadaThinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SevadaThinkApplication.class, args);
+    }
+
+}
