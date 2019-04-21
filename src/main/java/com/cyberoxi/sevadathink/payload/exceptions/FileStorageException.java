e98ae45035251e76cb84d45687a6d3431218b1ce
Amir <cnooed@gmail.com>
2019-04-21 17:29:11 -0400

    Completed Uploading File Controller and Service

---
+++ src/main/java/com/cyberoxi/sevadathink/payload/exceptions/FileStorageException.java
@@ -0,0 +1,11 @@
+package com.cyberoxi.sevadathink.payload.exceptions;
+
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
