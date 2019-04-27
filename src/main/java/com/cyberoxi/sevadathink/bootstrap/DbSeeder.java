a853be98ce06edfd2f7cbcf1899d47a1b11cc0aa
Amir <cnooed@gmail.com>
2019-04-27 11:30:45 -0400

    added plugins to pom

--- src/main/java/com/cyberoxi/sevadathink/bootstrap/DbSeeder.java
+++ src/main/java/com/cyberoxi/sevadathink/bootstrap/DbSeeder.java
@@ -7,6 +7,7 @@ import com.cyberoxi.sevadathink.model.User;
 import com.cyberoxi.sevadathink.repositories.CompanyRepository;
 import com.cyberoxi.sevadathink.repositories.EmployeeRepository;
 import com.cyberoxi.sevadathink.repositories.ProjectRepository;
+import com.cyberoxi.sevadathink.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -18,11 +19,13 @@ public class DbSeeder implements CommandLineRunner {
     private final EmployeeRepository employeeRepository;
     private final CompanyRepository companyRepository;
     private final ProjectRepository projectRepository;
+    private final UserRepository userRepository;
 
-    public DbSeeder(EmployeeRepository employeeRepository, CompanyRepository companyRepository, ProjectRepository projectRepository) {
+    public DbSeeder(EmployeeRepository employeeRepository, CompanyRepository companyRepository, ProjectRepository projectRepository, UserRepository userRepository) {
         this.employeeRepository = employeeRepository;
         this.companyRepository = companyRepository;
         this.projectRepository = projectRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -44,6 +47,12 @@ public class DbSeeder implements CommandLineRunner {
         user.setUsername("user");
         user.setPhoneNumber("555555555");
 
+        User user1 = new User();
+        user.setFirstName("user1");
+        user.setPassword("password");
+        user.setUsername("user1");
+        user.setPhoneNumber("1111111111");
+
         Project project = new Project();
         Project project1 = new Project();
 
@@ -51,30 +60,38 @@ public class DbSeeder implements CommandLineRunner {
         employee.setEmail("cnooed@gmail.com");
         employee.setCompany(company);
         employee.addProject(project);
+        employee.addProject(project1);
 
         company.setAddress("ffwegerhethertjherhergerwfgwe");
         company.setName("CyberOxi");
         company.addEmployee(employee);
         company.addEmployee(employee1);
         company.addProject(project);
+        company.addProject(project1);
 
         project.setCompany(company);
         project.setBudget("4444444444");
         project.addEmployee(employee);
+        project.addEmployee(employee1);
+        project.setOwner(user);
 
 
         employee1.setFirstName("Mohammad");
         employee1.setEmail("mmk@gmail.com");
         employee1.setCompany(company);
         employee1.addProject(project1);
+        employee1.addProject(project);
 
-        project1.setOwner(company);
+        project1.setCompany(company);
         project1.setBudget("555555555");
+        project1.addEmployee(employee);
         project1.addEmployee(employee1);
+        project1.setOwner(user1);
 
         employeeRepository.save(employee);
         projectRepository.save(project);
-
+        userRepository.save(user);
+        userRepository.save(user1);
         employeeRepository.save(employee1);
         projectRepository.save(project1);
         companyRepository.save(company);
